8ab48cd32db237565470e26d7104ec74cc5105f6
Kennedy Odhiambo <x.johnkennedy.x@gmail.com>
2024-06-15 20:18:41 +0300

    even numbers

---
+++ AscendingNumbers.java
@@ -0,0 +1,10 @@
+public class AscendingNumbers {
+    public static void main(String[] args) {
+        int number = 0;
+
+        while (number < 101) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
--- ScannerClass.java
+++ ScannerClass.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class ScannerClass {
     public static void main(String[] args) {
+        System.out.println("Enter your favourite integer");
         Scanner input = new Scanner(System.in);
-        System.out.println(input.next());
+        Integer value = input.nextInt();
+        System.out.println("Your favourite Integer is " + value);
     }
 }
